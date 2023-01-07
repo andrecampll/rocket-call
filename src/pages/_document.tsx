58efb9314750874b0165c8cf82bc19f164afad7d
andrecampll <andrevictor50@gmail.com>
2023-01-07 11:41:14 -0300

    chore: adds ESlint and next pageExtensions 🛠

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-        
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
         <style
           id="stitches"
           dangerouslySetInnerHTML={{
@@ -16,7 +23,7 @@ export default function Document() {
           }}
         />
       </Head>
-      
+
       <body>
         <Main />
         <NextScript />
