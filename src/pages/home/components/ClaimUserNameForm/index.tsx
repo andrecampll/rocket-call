4a1cae6a99e596c14ec8e155138236681ee7d11e
andrecampll <andrevictor50@gmail.com>
2023-01-16 06:44:48 -0300

    feat: adds React Hook Form ⚡️

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,11 +1,33 @@
 import { Button, TextInput } from '@rocket-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { z } from 'zod'
+
 import * as S from './styles'
 
+const claimUsernameSchema = z.object({
+  username: z.string().min(3).max(20),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameSchema>
+
 export const ClaimUserNameForm = () => {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  const handleClaimUsername = useCallback((data: ClaimUsernameFormData) => {
+    console.log(data)
+  }, [])
+
   return (
-    <S.Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="your-username" />
+    <S.Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="your-username"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Submit
         <ArrowRight />
