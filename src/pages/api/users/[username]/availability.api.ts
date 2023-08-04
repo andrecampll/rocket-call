bc84dad877f555aa127a597ddf9e4ca19161fa1a
andrecampll <andrevictor50@gmail.com>
2023-08-03 22:04:31 -0300

    feat: shows availability for each time

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -37,7 +37,7 @@ export default async function handle(
   const isPastDate = referenceDate.endOf('day').isBefore(new Date())
 
   if (isPastDate) {
-    return res.json({ availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const userAvailability = await prisma.userTimeInterval.findFirst({
@@ -48,7 +48,7 @@ export default async function handle(
   })
 
   if (!userAvailability) {
-    return res.json({ availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const { time_start_in_minutes, time_end_in_minutes } = userAvailability
@@ -60,5 +60,23 @@ export default async function handle(
     length: endHour - startHour,
   }).map((_, index) => startHour + index)
 
-  return res.json({ availability: possibleTimes })
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter(
+    (time) =>
+      !blockedTimes.some((blockedTime) => blockedTime.date.getHours() === time),
+  )
+
+  return res.json({ possibleTimes, availableTimes })
 }
