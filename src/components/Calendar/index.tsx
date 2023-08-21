983a55d1fb26d0c7e88a259ba2e17d16494bd511
andrecampll <andrevictor50@gmail.com>
2023-08-21 07:13:14 -0300

    feat: adds completed query for schedulings and times 🚀

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarProps = {
 
 type BlockedDates = {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 export const Calendar = ({ selectedDate, onDateChange }: CalendarProps) => {
@@ -60,7 +61,7 @@ export const Calendar = ({ selectedDate, onDateChange }: CalendarProps) => {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
 
@@ -104,7 +105,8 @@ export const Calendar = ({ selectedDate, onDateChange }: CalendarProps) => {
         date,
         disabled:
           date.endOf('day').isBefore(new Date()) ||
-          blockedDates?.blockedWeekDays.includes(date.get('day')),
+          blockedDates?.blockedWeekDays.includes(date.get('day')) ||
+          blockedDates.blockedDates.includes(date.get('date')),
       })),
       ...nextMonthFillArray.map((date) => ({ date, disabled: true })),
     ]
